77fa5c58fae2c7de65249c97cef15c562e144c23
Nic Bade <chipchapmaniii@gmail.com>
2017-08-06 15:44:32 -0500

    parsInt the formula calculator in app.js, now logs the total

--- app.js
+++ app.js
@@ -10,8 +10,11 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/input', function(req, res) {
-    var required = req.body;
-    calculator(required.inputOne, required.inputTwo, required.inputType);
+    console.log(req.body);
+    var inputOne = req.body.inputOne;
+    var inputTwo = req.body.inputTwo;
+    var inputType = req.body.inputType;
+    calculator(inputOne, inputTwo, inputType);
     console.log(math);
 })
 
@@ -26,13 +29,13 @@ app.listen(port, function() {
 
 function calculator(inputOne, inputTwo, inputType) {
     if (inputType == 'add') {
-        math = [inputOne + inputTwo];
+        math = [+inputOne + +inputTwo]; //+ =  parsInt
     } else if (inputType == 'subtract') {
-        math = [inputOne - inputTwo];
+        math = [+inputOne - +inputTwo];
     } else if (inputType == 'divide') {
-        math = [inputOne / inputTwo];
+        math = [+inputOne / +inputTwo];
     } else if (inputType == 'multiply') {
-        math = [inputOne * inputTwo];
+        math = [+inputOne * +inputTwo];
     }
     return math;
 }
--- public/client.js
+++ public/client.js
@@ -1,40 +1,41 @@
 console.log('JS sourced');
 
 $(document).ready(function() {
-        console.log('jQuery sourced');
-        getInput();
-        $('.button').on('click', function() {
+    console.log('jQuery sourced');
+    getInput();
+    $('.button').on('click', function() {
             console.log('Button clicked!');
             //pulls value of input cells and also the button type
             var inputOne = $('#inputOne').val();
             var inputTwo = $('#inputTwo').val();
             var inputType = $(this).attr('type');
-            //console.log(inputType);
+            // console.log(inputType);
             var inputObject = {
                 inputOne: inputOne,
                 inputTwo: inputTwo,
                 inputType: inputType,
             }
+            console.log(inputObject);
             $.ajax({
-                method: 'POST',
-                url: '/input',
-                data: inputObject,
-                success: function(response) {
-                    console.log('client.js POST', response);
-                    getInput();
-                }
-            })
-        })
-    })
-    // currently concatenates the array, need to parsint
+                    method: 'POST',
+                    url: '/input',
+                    data: inputObject,
+                    success: function(response) {
+                        console.log('client.js POST', response);
+                        getInput();
+                    }
+                }) //ajax post
+        }) // button click event listener
+})
+
 function getInput() {
     $.ajax({
-        method: 'GET',
-        url: '/input',
-        success: function(response) {
-            console.log('client.js GET', response);
-            $('#answer').prepend('<p>' + parsInt(response) + '</p>');
+            method: 'GET',
+            url: '/input',
+            success: function(response) {
+                console.log('client.js GET', response);
+                $('#answer').prepend('<p>' + (response) + '</p>');
 
-        }
-    })
-}
+            }
+        }) // end ajax get
+} // end getInput function
