95893b7c48e5b2dd0543d4869bfbdde4f98fae0c
Nic Bade <chipchapmaniii@gmail.com>
2017-08-05 21:27:05 -0500

    currenty contatinates the string.  need to parInt.  tomorrow

--- public/client.js
+++ public/client.js
@@ -1,37 +1,40 @@
 console.log('JS sourced');
 
 $(document).ready(function() {
-    console.log('jQuery sourced');
-
-    $('.button').on('click', function() {
-        console.log('Button clicked!');
-        //pulls value of input cells and also the button type
-        var inputOne = $('#inputOne').val();
-        var inputTwo = $('#inputTwo').val();
-        var inputType = $(this).attr('type');
-        //console.log(inputType);
-        var inputObject = {
-            inputOne: inputOne,
-            inputTwo: inputTwo,
-            inputType: inputType,
-        }
-        $.ajax({
-            method: 'POST',
-            url: '/input',
-            data: inputObject,
-            success: function(response) {
-                console.log('client.js POST', response);
+        console.log('jQuery sourced');
+        getInput();
+        $('.button').on('click', function() {
+            console.log('Button clicked!');
+            //pulls value of input cells and also the button type
+            var inputOne = $('#inputOne').val();
+            var inputTwo = $('#inputTwo').val();
+            var inputType = $(this).attr('type');
+            //console.log(inputType);
+            var inputObject = {
+                inputOne: inputOne,
+                inputTwo: inputTwo,
+                inputType: inputType,
             }
+            $.ajax({
+                method: 'POST',
+                url: '/input',
+                data: inputObject,
+                success: function(response) {
+                    console.log('client.js POST', response);
+                    getInput();
+                }
+            })
         })
     })
-})
-
+    // currently concatenates the array, need to parsint
 function getInput() {
     $.ajax({
         method: 'GET',
         url: '/input',
         success: function(response) {
             console.log('client.js GET', response);
+            $('#answer').prepend('<p>' + parsInt(response) + '</p>');
+
         }
     })
 }
