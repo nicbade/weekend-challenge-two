3dc111c85b1f5bd76f083d2a699f40355dda653a
Nic Bade <chipchapmaniii@gmail.com>
2017-08-05 20:55:30 -0500

    pulled values from the input fields and button type

--- public/client.js
+++ public/client.js
@@ -3,4 +3,18 @@ console.log('JS sourced');
 $(document).ready(function() {
     console.log('jQuery sourced');
 
+    $('.button').on('click', function() {
+        console.log('Button clicked!');
+        //pulls value of input cells and also the button type
+        var inputOne = $('#inputOne').val();
+        var inputTwo = $('#inputTwo').val();
+        var inputType = $(this).attr('type');
+        //console.log(inputType);
+        var inputObject = {
+            inputOne: inputOne,
+            inputTwo: inputTwo,
+            inputType: inputType,
+        }
+
+    })
 })
